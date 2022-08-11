90d31ba5791e604befd0d1984ce0d0f2cc930e7e
Matthieu <matthieunadeau96@gmail.com>
2022-08-11 14:18:36 -0400

    Add UI to TimerScreen

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -1,16 +1,59 @@
+import 'package:flow_time_2/widgets/action_buttons.dart';
+import 'package:flow_time_2/widgets/ticker.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flow_time_2/bloc/timer_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TimerScreen extends StatelessWidget {
   const TimerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Timer Screen'),
+    return BlocProvider(
+      create: (_) => TimerBloc(ticker: const Ticker()),
+      child: TimerView(),
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100),
+                child: Center(child: TimerText()),
+              ),
+              ActionButtons()
+            ],
+          )
+        ],
       ),
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesStr =
+        ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondStr = (duration % 60).floor().toString().padLeft(2, '0');
+
+    return Text(
+      '$minutesStr:$secondStr',
+      style: Theme.of(context).textTheme.headline1,
     );
   }
 }
